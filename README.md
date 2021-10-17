2453899d5fc13d94f14fa5baf3874c4306729579
JonathaCnB <jotacarlosftb@gmail.com>
2021-10-16 23:28:13 -0400

    introdução ao array e lifecycle methods

--- README.md
+++ README.md
@@ -6,10 +6,15 @@ a palavra run é usada é alguns casos.\
 
 **Note: estado são dados que o componente utiliza**
 
-**Note: Ambos os metodos estã corretos, porem o de baixa é mais usado**
+**Note: Ambos os métodos estão corretos, porem o de baixo é mais usado**
 
 **Note: A chamada da função pode ser feita das duas formas, sendo a segunda a mais comum**
 
 **Note: 'bind' é passar os valores de um componente para outro**
 
 **Note: 'arrowfunctions' não precisam da declaração bind da linha 8**
+
+### State com arrays
+
+Todo array precisa de um atributo `key` para otimizar a performasse
+**Note: Essa key precisa ser um dado único do elemento que está sendo retornado.**
--- src/App.js
+++ src/App.js
@@ -1,58 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.handlePClick = this.handlePClick.bind(this); //TODO: linha 13
-  //   this.state = {
-  //     name: 'Jonatha Carlos',
-  //     counter: 0
-  //   }
-  // }
-
   state = {
-    name: 'Jonatha Carlos',
-    counter: 0
-  }
+    posts: [ ]
+  };
 
-  handlePClick = () => {
-    const { name } = this.state
-    console.log(`Click ${ name }`)
-    this.setState({name:'Carneiro'})
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1});
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        posts: [
+          {
+            id: 1,
+            title: 'O titulo 1',
+            body: 'O corpo 1'
+          },
+          {
+            id: 2,
+            title: 'O titulo 2',
+            body: 'O corpo 2'
+          },
+          {
+            id: 3,
+            title: 'O titulo 3',
+            body: 'O corpo 3'
+          },
+        ]
+      })
+    }, 5000)
   }
 
   render() {
-    //const name = this.state.name;
-    //TODO: Linha 9
-    const {name, counter} = this.state;
+    const { posts } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {/* <p onClick={() => console.log('Clico')}>
-            {name} TODO: Linha 11
-          </p> */}
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* Apenas para uma linha */}
+        {/* {posts.map(post => <h1 key={post.id}>{post.title}</h1>)} */}
+        
+        {/* Para mais de uma linha */}
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
